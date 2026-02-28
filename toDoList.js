320348ff627d29fe16bf2b5539e0c9b72e134941
muthuselvan19 <selvammuthu84972@gmail.com>
2026-02-28 15:49:39 +0530

    Page Modification

--- toDoList.js
+++ toDoList.js
@@ -1,18 +1,21 @@
 let taskSaver = new Array();
 
-let task_value = document.getElementById('task');
+let task_val = document.getElementById('task');
 let submitoption = document.getElementById('submmission');
 let task = document.getElementById('list_of_operation');
+let clearTag = document.getElementById('clearing');
 
 submitoption.addEventListener('click', (event) => {
-    if (task_value.value != '') {
-        taskSaver.push(task_value.value);
-        task.innerHTML += '<li id = "' + (taskSaver.length - 1) + '"> <button onclick="overTask(' + (taskSaver.length - 1) + ')">' + task_value.value + '</button></li>'
-        task_value.value = '';
+    if (task_val.value != '') {
+
+        taskSaver.push(task_val.value);
+        task.innerHTML += '<li id = "' + (taskSaver.length - 1) + '"> <button onclick="overTask(' + (taskSaver.length - 1) + ')">' + task_val.value + '</button></li>'
+        task_val.value = '';
+
     }
 
     else {
-        alert("invalid task (or) Empty task")
+        alert("invalid task (or) Empty task");
     }
 });
 
@@ -21,3 +24,15 @@ function overTask(id) {
     taskCompletion.innerHTML += '<li>' + taskSaver[id] + '</li>'
 
 }
+
+const now = new Date().toLocaleTimeString();
+    // innerHTML = '<p>'+ now +'</p>'
+    // console.log(now);
+
+// let timing = document.getElementById('op');
+// if(task_val.value == ''){
+//     timing.innerHTML += now;
+// }
+// else{
+//     timing.innerHTML = "Somthing went Wrong"
+// }
